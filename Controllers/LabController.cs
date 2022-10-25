66e628b774d6b01faad73ae443b115c6e341306b
isabella-66 <amasprog@gmail.com>
2022-10-25 03:13:05 -0300

    feat: add lab index

---
+++ Controllers/LabController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using MvcLabManager.Models;
+
+namespace MvcLabManager.Controllers;
+
+public class LabController : Controller
+{
+    private readonly LabManagerContext _context;
+
+    public LabController(LabManagerContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index() => View(_context.Labs);
+}
--- Models/LabManagerContext.cs
+++ Models/LabManagerContext.cs
@@ -5,6 +5,7 @@ namespace MvcLabManager.Models;
 public class LabManagerContext : DbContext //n usa direto o dbcontext pq está usando algo novo, o dbset dos models
 {
     public DbSet<Computer> Computers { get; set; }
+    public DbSet<Lab> Labs { get; set; }
 
     //base é p/ chamar construtor da superclasse, do parent. aqui os options são passados pro base, que é o constutor do context
     public LabManagerContext(DbContextOptions<LabManagerContext> options) : base(options) //conjunto de opções pro context, só que apenas para o lab manager em específico
